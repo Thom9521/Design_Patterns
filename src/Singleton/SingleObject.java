0403080d2e49be397141beee7a3a467a851f1a8a
Thom9521 <Thom9521@edu.easj.dk>
2018-03-08 09:08:38 +0100

    Initial commit

---
+++ src/Singleton/SingleObject.java
@@ -0,0 +1,20 @@
+package Singleton;
+
+public class SingleObject {
+
+    //Lavet et objekt af SingleObject
+    private static SingleObject instance = new SingleObject();
+
+    //Laver en private constructor
+    private SingleObject(){}
+
+    //Getter metode til instance
+    public static SingleObject getInstance(){
+        return instance;
+    }
+
+    //Metode som printer en besked
+    public void showMessage(){
+        System.out.println("Hello World!");
+    }
+}
