0bcbaf5656a95bd7151b43514d1ca9a319b02183
Thom9521 <Thom9521@edu.easj.dk>
2018-03-08 09:48:45 +0100

    Kommentar

---
+++ src/Iterator/NameRepository.java
@@ -0,0 +1,32 @@
+package Iterator;
+
+public class NameRepository implements Container {
+    public String names[] = {"Michael", "Lisa", "Niels", "Thomas"};
+
+    @Override
+    public Iterator getIterator(){
+        return new NameIterator();
+    }
+
+    private class NameIterator implements Iterator{
+        int index;
+
+        //Metode til at tjekke om der er flere navne i listen
+        @Override
+        public boolean hasNext(){
+            if (index < names.length){
+                return true;
+            }
+            return false;
+        }
+
+        //Metode til at udskrive alle navne i listen
+        @Override
+        public Object next(){
+            if (this.hasNext()){
+                return names[index++];
+            }
+            return null;
+        }
+    }
+}
